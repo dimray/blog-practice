5bda6e8b70f84dc9f0893b9ac51a9dac814db9de
dimray <dimray@gmail.com>
2023-11-25 11:26:30 +0000

    Eleventified

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/style.css");
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "public",
+      includes: "_includes",
+      layouts: "_layouts",
+    },
+  };
+};
